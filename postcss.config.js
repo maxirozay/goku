94a3ad02d5864b3136fc850bca9a50b68a0bd7ab
Maxime Rossier <rossier.maxime@gmail.com>
2018-03-15 17:40:51 +0100

    lint before import

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = () => ({
   plugins: {
-    'postcss-import': {},
     stylelint: {},
+    'postcss-import': {},
     'postcss-cssnext': {},
     cssnano: { autoprefixer: false }
   }
