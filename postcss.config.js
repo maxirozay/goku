e5f6043ac97176121bccedc55f528d9f5cf051f4
Maxime Rossier <rossier.maxime@gmail.com>
2018-03-15 10:37:23 +0100

    use cssnext

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = () => ({
   plugins: {
     'postcss-import': {},
     stylelint: {},
-    autoprefixer: {},
-    cssnano: {}
+    'postcss-cssnext': {},
+    cssnano: { autoprefixer: false }
   }
 })
